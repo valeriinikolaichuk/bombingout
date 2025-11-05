5965e0667e8656a1e76bad840bebee425abb4163
valeriinikolaichuk <powerlift.rv@gmail.com>
2025-11-05 12:49:51 +0100

    changes

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -10,12 +10,24 @@
     class MainController extends AbstractController {
         #[Route('/', name: 'home')]
         public function index(Request $request, SessionInterface $session): Response {
-            $error = $session->has('error_log_in') ? $session->get('error_log_in') : null;
-            $session->remove('error_log_in');
-                
-            return $this -> render('login_page.html.twig', [
-                'error_log_in' => $error,
-            ]);
+            if (!$session -> has('id') || !$session -> has('status') || !$session -> has('language')) {
+
+                $error = $session -> has('error_log_in') ? $session -> get('error_log_in') : null;
+                $session->remove('error_log_in');
+                    
+                return $this -> render('login_page.html.twig', [
+                    'error_log_in' => $error,
+                ]);
+            }
+
+            if ($session -> get('status') !== 'participant') {
+                if ($request -> request -> has('comp_status') || $session->has('comp_status')) {
+                    $setStatus = new SetStatus();
+                } else {
+                    return $this->render('redirection_page.html.twig');
+                }
+            } 
+
         }
     }
 ?>
