9e6acf3474af9011ecaf9b5d8a34589d25575c2d
valeriinikolaichuk <powerlift.rv@gmail.com>
2025-11-20 21:13:18 +0100

    del

--- backend/public/assets/view/view_MainPage/viewNomin.js
+++
@@ -1,5 +0,0 @@
-function ViewNomin(){
-    this.newPreliminaryDate = document.getElementById('preliminaryDate');
-    this.newFinalDate = document.getElementById('finalDate');
-    this.start_dateOnline = document.getElementById('start_dateOnline');
-}
