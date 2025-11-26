1599c66201da069e3da8c61912c4429750e1fc83
valeriinikolaichuk <powerlift.rv@gmail.com>
2025-11-26 22:38:01 +0100

    rename

---
+++ backend/public/assets/js/login/preventReloginButtons.js
@@ -0,0 +1,48 @@
+const dialogContinue = document.getElementById("dialogContinue");
+dialogContinue.addEventListener('click', () => {
+    document.getElementById("loginDialog").close();
+});
+
+const dialogDelete = document.getElementById("dialogDelete");
+dialogDelete.addEventListener('click', async  function() {
+    let data = {
+        usersId: document.getElementById("usersId").value,
+        usersIp: document.getElementById("usersIp").value,
+        usersAgent: document.getElementById("usersAgent").value
+    };
+
+    let response = await fetch("/api/delPreviousReg", {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(data)
+    });
+
+    if (!response.ok){
+        throw new Error('error by path: /api/delPreviousReg');
+    }
+
+    let json = await response.json();
+
+    document.getElementById("loginDialog").close();
+
+    let mess;
+    if (json.success){
+        if (lang === 'uk'){
+            mess = 'запис видалено';
+        } else if (lang === 'pl'){
+            mess = 'wpis został usunięty';
+        } else {
+            mess = 'entry deleted';
+        }
+    } else {
+        if (lang === 'uk'){
+            mess = 'помилка видалення';
+        } else if (lang === 'pl'){
+            mess = 'błąd usuwania';
+        } else {
+            mess = 'deletion error';
+        } 
+    }
+
+    alert(mess);
+});
