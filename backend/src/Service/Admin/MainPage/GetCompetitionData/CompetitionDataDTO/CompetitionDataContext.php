00f36494a06cc267a726c968581db8a459f70c51
valeriinikolaichuk <powerlift.rv@gmail.com>
2026-02-21 20:29:30 +0100

    new

---
+++ backend/src/Service/Admin/MainPage/GetCompetitionData/CompetitionDataDTO/CompetitionDataContext.php
@@ -0,0 +1,9 @@
+<?php
+    namespace App\Service\Admin\MainPage\GetCompetitionData\CompetitionDataDTO;
+
+    class CompetitionDataContext{
+        public ?string $dataType = null;
+        public ?int $usersId = null;
+        public ?int $compID = null;
+    }
+?>
