7a523a6ec1181851ce172774ae55cba7fdcab803
valeriinikolaichuk <powerlift.rv@gmail.com>
2026-02-26 18:26:04 +0100

    del

--- backend/assets/bootstrap.js
+++
@@ -1,6 +0,0 @@
-import { startStimulusApp } from '@symfony/stimulus-bundle';
-import '@hotwired/turbo';
-
-const app = startStimulusApp();
-// register any custom, 3rd party controllers here
-// app.register('some_controller_name', SomeImportedController);
